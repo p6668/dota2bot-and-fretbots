40c895eece14c44f76f5b90e1a51175e5a6b1759
p6668 <you@example.com>
2024-01-19 00:54:39 -0500

    default diff to 8

--- vscripts/Settings.lua
+++ vscripts/Settings.lua
@@ -34,7 +34,7 @@ local votingTimeElapsed = -1
 -- The playerID of the host.  Used to whitelist chat commands.
 local hostID = -1
 -- default difficulty if no one votes
-noVoteDifficulty = 5
+noVoteDifficulty = 8
 -- Is repurcussion timer started?
 local isRepurcussionTimerStarted = false
 -- Instantiate ourself
