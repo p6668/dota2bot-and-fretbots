5daeb7386030b32b6c91a881e81aa79fa0f4963c
Tianxing Li <litianx2@msu.edu>
2026-01-25 02:44:43 -0500

    Update Stats.lua

--- vscripts/bots/Buff/Stats.lua
+++ vscripts/bots/Buff/Stats.lua
@@ -9,37 +9,38 @@ end
 function Stats.UpdateStats(bot, nTeam)
     local gameTime = Helper.DotaTime() / 60
     local botPos = Helper.GetPosition(bot, nTeam)
+    local unitStats = 1/60
 
-    if gameTime >= 10 and gameTime <=30 then
+    if gameTime >= 10 and gameTime <=30 then 
         local stat
-        local bonus = 0.01667
+        local bonus = unitStats * 1.3 -- add 1 stats per min 
         stat = bot:GetBaseStrength()
         bot:SetBaseStrength(stat + bonus)
         stat = bot:GetBaseAgility()
         bot:SetBaseAgility(stat + bonus)
         stat = bot:GetBaseIntellect()
         bot:SetBaseIntellect(stat + bonus * 0.1) -- reduce int stats bonus due to 7.33 update giving magic resist
-    elseif gameTime > 30 and gameTime <=40 then 
+    elseif gameTime > 30 and gameTime <=40 then
         local stat
-        local bonus = 0.01667*1.5
+        local bonus = unitStats * 1.5 -- add 1.5 stats per min
         stat = bot:GetBaseStrength()
         bot:SetBaseStrength(stat + bonus)
         stat = bot:GetBaseAgility()
         bot:SetBaseAgility(stat + bonus)
         stat = bot:GetBaseIntellect()
         bot:SetBaseIntellect(stat + bonus * 0.1) -- reduce int stats bonus due to 7.33 update giving magic resist
-    elseif gameTime > 40 and gameTime <=60 then 
+    elseif gameTime > 40 and gameTime <=60 then  
         local stat
-        local bonus = 0.01667*2
+        local bonus = unitStats * 2 -- add 2 stats per min
         stat = bot:GetBaseStrength()
         bot:SetBaseStrength(stat + bonus)
         stat = bot:GetBaseAgility()
         bot:SetBaseAgility(stat + bonus)
         stat = bot:GetBaseIntellect()
         bot:SetBaseIntellect(stat + bonus * 0.1) -- reduce int stats bonus due to 7.33 update giving magic resist
-    elseif gameTime > 60 and gameTime <=61 then -- god mode +100 str, agi, and int.
+    elseif gameTime > 60 and gameTime <=61 then
         local stat
-        local bonus = 1.667
+        local bonus = unitStats * 100 -- god mode +100 str, agi, and int.
         stat = bot:GetBaseStrength()
         bot:SetBaseStrength(stat + bonus)
         stat = bot:GetBaseAgility()
